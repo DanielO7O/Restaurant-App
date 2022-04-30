24700889fbec2ada537f6385cb54d82b6f5a0e6d
DanielO7O <dynamikdaniel@gmail.com>
2022-05-01 00:49:25 +0100

    maino

---
+++ App.js
@@ -0,0 +1,29 @@
+
+
+
+import CV from './CV_Assignment/CV';
+
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="ass-header" >
+        <h1 className='ass-title' style={{
+          color:'green',
+          display:'flex',
+          padding:'5px',
+          justifyContent:'center',
+        }}> SH REACT 5.0 </h1>
+        <h3 className='work'> CV ASSIGNMENT </h3>
+      </header>
+        <CV />
+      
+    </div>
+    
+  );
+}
+
+
+export default App;
